67f4a484dfee1f89d89b1bc6e81b4f7272b5628b
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-10-16 18:20:47 -0400

    0.0.15

--- be-functional.ts
+++ be-functional.ts
@@ -1,4 +1,4 @@
-import {define, BeDecoratedProps} from 'be-decorated/be-decorated.js';
+import {define, BeDecoratedProps} from 'be-decorated/DE.js';
 import {Proxy, PP, Actions, VirtualProps, FnParam} from './types';
 import {register} from 'be-hive/register.js';
 
